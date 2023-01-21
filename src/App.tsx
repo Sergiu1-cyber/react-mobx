b65e1a6760f39f12bcb53f1a4cc139289994ee39
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-21 10:44:11 +0200

    add mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {observer} from 'mobx-react-lite'
+import Counter from './store/Counter'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = observer(() => {
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-red-500">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>React + Mobx</h1>
+      <h1 className='text-red-500'>Count: {Counter.count}</h1>
+      <br/>
+      <button onClick={() => Counter.increment()}>incr</button>
+      <br/>
+      <button onClick={() => Counter.decrement()}>decr</button>
     </div>
   )
-}
+})
 
 export default App
